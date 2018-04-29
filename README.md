f46f5c6fce003fa3b2b58de39c9fc14a19cda161
Gregorio Galante <gregogalante@gmail.com>
2018-04-29 11:45:37 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# PHP Facebook posts
+
+A simplest script to get last posts from Facebook pages.
+
+## Usage
+
+1. Clone the repositiory.
+2. Edit the ```config.ini``` file with your configuration.
+3. Run the ```index.php``` script.
+
+## Note
+
+Facebook has changed the policy used to retrieve page informations so this script should work only with Facebook applications created before the 02-2018.
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = parse_ini_file("config.ini");
+
+function _fetchUrl($url) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, $config["curl_timeout"]);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $config["curl_sslverifypeer"]);
+  $feedData = curl_exec($ch);
+  curl_close($ch);
+  return $feedData;
+}
+
+$request = _fetchUrl("https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id={$config["facebook_app_id"]}&client_secret={$config["facebook_secret_id"]}");
+$response = json_decode($request);
+$posts = _fetchUrl("https://graph.facebook.com/{$config["facebook_page_id"]}/posts?access_token={$response->access_token}&fields={$config["facebook_fields"]}");
+
+echo $posts;
+
+?>
