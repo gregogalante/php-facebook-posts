16a70234a5ac066a583ead2562db322bd8b2ffa1
Francesco Allegrini <cesco9525@gmail.com>
2019-01-16 10:31:46 +0100

    global config

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once('./config.php');
 
 function _fetchUrl($url) {
+  global $config;
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
